faf5229d793f83060d5e2b77f04f09bdc20d4b5e
stefanbpeterson <stefanbpeterson@gmail.com>
2021-12-01 22:40:00 -0700

    refactoring code to use state to reference character data

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,12 +1,20 @@
-import './App.css';
-import Card from '../../Components/Card/Card'
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <Card />
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      nintendoCharacters: [
+        {name: 'Mario', image: '', sounds: [], id: 1}, 
+        {name: 'Luigi', image: '',sounds: [], id: 2}
+      ]
+    }
+  }
+  render() {
+    return (
+      <h1>This is a test.</h1>
+    )
+  }
 }
 
-export default App;
+export default App
