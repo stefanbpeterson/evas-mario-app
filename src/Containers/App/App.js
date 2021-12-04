50d2bb183233d3701eaad10cf1fa68f28237a886
stefanbpeterson <stefanbpeterson@gmail.com>
2021-12-04 16:41:43 -0700

    adding new characters

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Mario from '../../Assets/Images/mario.png'
 import Luigi from '../../Assets/Images/luigi.png'
+import Peach from '../../Assets/Images/peach.png'
+import Daisy from '../../Assets/Images/daisy.png'
 import './App.css'
 
 export class App extends Component {
@@ -9,7 +11,9 @@ export class App extends Component {
     this.state = {
       nintendoCharacters: [
         {name: 'Mario', image: <img className={'characterImage'} src={Mario} alt='Mario' />, sounds: [], id: 1},
-        {name: 'Luigi', image: <img className={'characterImage'} src={Luigi} alt='Luigi' />, sounds: [], id: 2}
+        {name: 'Luigi', image: <img className={'characterImage'} src={Luigi} alt='Luigi' />, sounds: [], id: 2},
+        {name: 'Peach', image: <img className={'characterImage'} src={Peach} alt='Peach' />, sounds: [], id: 3},
+        {name: 'Daisy', image: <img className={'characterImage'} src={Daisy} alt='Daisy' />, sounds: [], id: 4}
       ]
     }
   }
@@ -18,6 +22,7 @@ export class App extends Component {
       <div>
         {this.state.nintendoCharacters[0].image}
         {this.state.nintendoCharacters[1].image}
+        {this.state.nintendoCharacters[2].image}
       </div>
 
     )
